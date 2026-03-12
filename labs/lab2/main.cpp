def6ffab9c5f85b65c7992495807aecb55d92bde
Saqlain-mushtaq-alamin <alamin86911@gmail.com>
2026-03-12 16:21:03 +0600

    Add main function and graphics setup for star drawing in Lab2

--- labs/lab2/main.cpp
+++ labs/lab2/main.cpp
@@ -1,1 +1,34 @@
+ #include <graphics.h>
+#include <conio.h>
+
+int main()
+{
+    int gd = DETECT, gm;
+
  
+    initgraph(&gd, &gm, "C:\\Turboc3\\BGI");
+
+    // Star coordinates
+    int points[] = {
+        200,100,
+        220,160,
+        280,160,
+        230,200,
+        250,260,
+        200,220,
+        150,260,
+        170,200,
+        120,160,
+        180,160
+    };
+
+    // Set fill color
+    setfillstyle(SOLID_FILL, YELLOW);
+
+    // Draw and fill the star
+    fillpoly(10, points);
+
+    getch();
+    closegraph();
+    return 0;
+}
